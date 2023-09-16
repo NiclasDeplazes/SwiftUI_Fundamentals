8f907229d10f35530ede2e874de0b7a600ad8560
Niclas Deplazes <niclas.deplazes@bluewin.ch>
2023-09-16 19:22:31 +0200

    Adds Grid and DetailView

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by Niclas on 16.09.23.
+//
+
+import SwiftUI
+
+final class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
+}
